a0507330ca053739fe68fb71e122843fd36576b4
Aeneas Stankowski <aeneas@xhdr.com>
2023-04-13 17:40:04 +0200

    many things

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,10 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		fontFamily: {
+			'sans': ['area-normal', 'sans'],
+			'public': ['Public Sans', 'sans-serif']
+		  },
 		extend: {
 			colors: {
 			  blue: {
