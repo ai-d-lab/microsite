677bdba3480fdb25e1053fc0f5b9544a10335d9f
Christopher Pietsch <cpietsch@gmail.com>
2023-04-18 16:26:50 +0200

    added svelte and dynamic logo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import svelte from '@astrojs/svelte';
 
 import tailwind from "@astrojs/tailwind";
 
@@ -8,5 +9,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   site: "https://ai-d-lab.github.io",
   base: "/",
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap(), tailwind(), svelte()],
 });
---
+++ src/components/Logo.svelte
@@ -0,0 +1,8 @@
+<script>
+    // logos are named 1.png, 2.png, 3.png, etc. until 8.png
+    const random = Math.floor(Math.random() * 8) + 1
+    const src = `/logo/${random}.png`
+
+</script>
+
+<img src={src} class="mx-auto w-80 md:w-96 mt-4">
