b7871db6a387d436efcf724720485b4f1c98ff17
Christopher Pietsch <cpietsch@gmail.com>
2023-04-18 10:25:37 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://ai-d-lab.github.io",
-  base: "/ai-d-website",
+  base: "/",
   integrations: [mdx(), sitemap(), tailwind()],
 });
