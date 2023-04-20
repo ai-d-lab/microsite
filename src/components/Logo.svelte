a5b05c46f1f82fa7bccad3fe4158df0ea778aa76
Aeneas Stankowski <aeneas@xhdr.com>
2023-04-20 22:31:42 +0200

    undo svelte

--- src/components/Logo.svelte
+++ src/components/Logo.svelte
@@ -1,7 +1,10 @@
 <script>
     // logos are named 1.png, 2.png, 3.png, etc. until 8.png
     const random = Math.floor(Math.random() * 8) + 1
+
     const src = `/logo/${random}.png`
+
+    console.log(random)
 
 </script>
 
