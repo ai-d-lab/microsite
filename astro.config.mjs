557f4a9094424dfe7fd9b1c94388ded6bcb32d58
Aeneas Stankowski <aeneas@xhdr.com>
2023-04-20 23:22:37 +0200

    remove svelte and replace with astro component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import svelte from '@astrojs/svelte';
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://ai-d-lab.github.io",
   base: "/",
-  integrations: [mdx(), sitemap(), tailwind(), svelte()],
+  integrations: [mdx(), sitemap(), tailwind()],
 });
