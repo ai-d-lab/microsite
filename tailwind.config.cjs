9db37ed856511b4748df8bbc54aba94aba565d88
Christopher Pietsch <cpietsch@gmail.com>
2023-05-08 22:09:41 +0200

    added tailwind typography plugin

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,23 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	darkMode: "class",
-	theme: {
-		fontFamily: {
-			'sans': ['area-normal', 'sans'],
-			'public': ['Public Sans', 'sans-serif']
-		  },
-		
-		extend: {
-			colors: {
-			  blue: {
-				950: '#633A19',
-			  },
-			}
-		  },
-		},
-	plugins: [
-		require("tailwind-gradient-mask-image"),
-		require('@tailwindcss/line-clamp'),
-	],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
+  theme: {
+    fontFamily: {
+      sans: ["area-normal", "sans"],
+      public: ["Public Sans", "sans-serif"],
+    },
+
+    extend: {
+      colors: {
+        blue: {
+          950: "#633A19",
+        },
+      },
+    },
+  },
+  plugins: [
+    require("tailwind-gradient-mask-image"),
+    require("@tailwindcss/line-clamp"),
+    require("@tailwindcss/typography"),
+  ],
+};
