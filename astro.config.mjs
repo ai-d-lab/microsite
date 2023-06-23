d7b71410fefd29b032d235b02a9d8175431f1083
Christopher Pietsch <cpietsch@gmail.com>
2023-06-23 13:36:25 +0200

    www

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://goosplit.com",
+  site: "https://www.goosplit.com",
   base: "/",
   experimental: {
     assets: true,
