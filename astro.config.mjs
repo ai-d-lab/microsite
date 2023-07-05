cccb19fd4f22505f4744b6eb64bda53af704e0dc
Alexa Steinbrück <5918060+alexabruck@users.noreply.github.com>
2023-07-05 19:10:14 +0200

    Remove @astrojs/image, use futureproof astro:assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig, sharpImageService } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
@@ -13,9 +12,6 @@ export default defineConfig({
     assets: true,
   },
   integrations: [
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
     mdx(),
     sitemap(),
     tailwind({
