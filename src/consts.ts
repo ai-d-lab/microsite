bdc9c07ced2aace7b0955c89e8e676975667719b
Alexa Steinbrück <5918060+alexabruck@users.noreply.github.com>
2023-07-11 14:40:20 +0200

    Consistent spelling of AI+D (no whitespace)

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "AI + D Lab";
+export const SITE_TITLE = "AI+D Lab";
 export const SITE_DESCRIPTION =
   "The AI+D Lab is the place at HfG Schwäbisch Gmünd for research and teaching that combines artificial intelligence and design.";
