7815b4eca37e44d7768383086d90893fc4fe813e
Alexa Steinbrück <5918060+alexabruck@users.noreply.github.com>
2023-07-11 12:41:01 +0200

    Change site description

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'AI + D Lab';
-export const SITE_DESCRIPTION = 'AI + D Lab at HfG Schwäbisch Gmünd';
+export const SITE_TITLE = "AI + D Lab";
+export const SITE_DESCRIPTION =
+  "The AI+D Lab is the place at HfG Schwäbisch Gmünd for research and teaching that combines artificial intelligence and design.";
