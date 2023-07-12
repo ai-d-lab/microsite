fb9259a3391c3dff53e35af306cbb87415d405c3
Alexa Steinbrück <5918060+alexabruck@users.noreply.github.com>
2023-07-12 14:02:56 +0200

    Clean up images folder

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,26 +2,29 @@ import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
   // Type-check frontmatter using a schema
-  schema: z.object({
-    title: z.string(),
-    subtitle: z.string(),
-    description: z.string(),
-    draft: z.boolean().optional(),
-    author: z.string(),
-    showintro: z.boolean().optional(),
-    // Transform string to Date object
-    pubDate: z
-      .string()
-      .or(z.date())
-      .transform((val) => new Date(val)),
-    updatedDate: z
-      .string()
-      .optional()
-      .transform((str) => (str ? new Date(str) : undefined)),
-    heroImage: z.string().optional(),
-    heroVideo: z.string().optional(),
-    appearance: z.enum(["S", "M", "L"]),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      subtitle: z.string(),
+      description: z.string(),
+      draft: z.boolean().optional(),
+      author: z.string(),
+      showintro: z.boolean().optional(),
+      // Transform string to Date object
+      pubDate: z
+        .string()
+        .or(z.date())
+        .transform((val) => new Date(val)),
+      updatedDate: z
+        .string()
+        .optional()
+        .transform((str) => (str ? new Date(str) : undefined)),
+      heroImage: image().refine((img) => img.width >= 600, {
+        message: "Cover image must be at least 600 pixels wide!",
+      }),
+      heroVideo: z.string().optional(),
+      appearance: z.enum(["S", "M", "L"]),
+    }),
 });
 
 // same as blog posts, but only newest one is displayed on the top of the page
